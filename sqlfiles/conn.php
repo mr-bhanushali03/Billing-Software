d0ba66f34ef18636432cf7b44668fd79caffd1a6
mrbhanushali03 <mrbhanushali03@mrbhanushali03.mrbhanushali03>
2023-12-08 14:55:16 +0530

    minor changes

--- sqlfiles/conn.php
+++ sqlfiles/conn.php
@@ -1,14 +1,8 @@
 <?php
-	
-	// connect to mysqli
-	$conn = mysqli_connect("localhost","root","Kev@l8347","saral2") or die("Sorry, can't connect to the mysqli.");
-	
-	$current_date_time=date('Y-m-d H:i:s'); //mysqli format with date 
 
-	  $current_date=date('Y-m-d'); //mysqli format with Current date 
+// connect to mysqli
+$conn = mysqli_connect("localhost", "root", "Kev@l8347", "saral2") or die("Sorry, can't connect to the mysqli.");
 
-?>
+$current_date_time = date('Y-m-d H:i:s'); //mysqli format with date 
 
-
-
-
+$current_date = date('Y-m-d'); //mysqli format with Current date 
